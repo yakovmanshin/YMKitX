95ec906aedecddd064c758d9cd5c5552b65c4eb0
Yakov Manshin <git@yakovmanshin.com>
2026-03-15 01:29:24 +0000

    Updated `MonitoringServiceTests`

--- Monitoring/test/suites/MonitoringServiceTests.swift
+++ Monitoring/test/suites/MonitoringServiceTests.swift
@@ -25,22 +25,47 @@ struct MonitoringServiceTests {
         )
     }
     
-    @Test(arguments: [nil, "TEST_Custom Category"]) func log(customCategoryName: String?) async {
+    @Test(arguments: [
+        (nil, .default),
+        ("TEST_Custom Category 1", .info),
+        ("TEST_CustomCategory2", .fault),
+        ("TEST_Cat3", .debug),
+    ] as [(String?, LogLevel)]) func log(category: String?, level: LogLevel) async {
         let message = "TEST_Log Message"
-        if let customCategoryName {
-            await service.log(forCategory: customCategoryName, message)
-        } else {
-            await service.log(message)
-        }
+        
+        await service.log(category: category, level: level, message)
         
         #expect(loggingService.log_invocationCount == 1)
-        #expect(loggingService.log_levels == [.default])
+        #expect(loggingService.log_levels == [level])
         #expect(loggingService.log_messages == [message])
-        if let customCategoryName {
-            #expect(loggingService.log_categories == [.custom(customCategoryName)])
+        if let category {
+            #expect(loggingService.log_categories == [.custom(category)])
         } else {
             #expect(loggingService.log_categories == [.default])
         }
+    }
+    
+    @Test func log_defaultCategory() async {
+        let message = "TEST_Log Message"
+        
+        await service.log(message)
+        
+        #expect(loggingService.log_invocationCount == 1)
+        #expect(loggingService.log_categories == [.default])
+        #expect(loggingService.log_levels == [.default])
+        #expect(loggingService.log_messages == [message])
+    }
+    
+    @Test func log_customCategory() async {
+        let category = "TEST_CustomCategory"
+        let message = "TEST_Log Message"
+        
+        await service.log(forCategory: category, message)
+        
+        #expect(loggingService.log_invocationCount == 1)
+        #expect(loggingService.log_categories == [.custom(category)])
+        #expect(loggingService.log_levels == [.default])
+        #expect(loggingService.log_messages == [message])
     }
     
     @Test(arguments: [nil, "TEST_Custom Category"]) func logSync(customCategoryName: String?) async throws {
